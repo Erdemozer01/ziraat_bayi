75f57e48feb0fcbc6586293218a8e770710b431b
Erdemozer01 <ozer246@gmail.com>
2025-01-21 21:36:47 +0300

    fixed

--- bayi/views.py
+++ bayi/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from django.views import generic
 from .models import ProductCategory, Product
-
+from django.contrib import messages
 
 class CategoriesListView(generic.ListView):
     model = ProductCategory
@@ -19,4 +19,5 @@ class ProductListView(generic.ListView):
         ara = self.request.GET.get('ara', None)
         if ara:
             object_list = object_list.filter(Q(name__icontains=ara) | Q(category__name=ara))
+            messages.success(self.request, f'{len(object_list)} ürün bulundu')
         return object_list
