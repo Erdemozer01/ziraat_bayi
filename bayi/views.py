f19a12658bd5720ee2bab4376fa13838018d5d15
Erdemozer01 <ozer246@gmail.com>
2025-01-21 20:45:18 +0300

    fixed

--- bayi/views.py
+++ bayi/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views import generic
 from .models import ProductCategory, Product
 
@@ -12,3 +13,11 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = 'pages/bayi.html'
     paginate_by = 10
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        object_list = Product.objects.all()
+        ara = self.request.GET['ara']
+        if ara:
+            object_list = object_list.filter(Q(name__icontains=ara) | Q(category__name=ara))
+        return object_list
