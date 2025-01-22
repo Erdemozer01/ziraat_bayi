8ea370c69b438ac254be50db871366c288256fc0
Erdemozer01 <ozer246@gmail.com>
2025-01-22 14:11:54 +0300

    fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('<user>/', views.DashboardView.as_view(), name='dashboard'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('kayıt-ol', views.RegisterView.as_view(), name='register'),
     path('accounts/password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
