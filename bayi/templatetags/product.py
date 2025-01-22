45a0820e5c83549a8af2c552d57bf1f6cff53b73
Erdemozer01 <ozer246@gmail.com>
2025-01-22 12:53:57 +0300

    fixed

--- bayi/templatetags/product.py
+++ bayi/templatetags/product.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag
 def get_product_category():
     return ProductCategory.objects.all()
+
+
+@register.filter(name='breadcrumbs')
+def breadcrumbs(value):
+    return value.rsplit('/', 2)[1].replace('-', ' ')
