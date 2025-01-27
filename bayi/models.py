4520b27607f26aa17376d41cc7002c31a2ea045d
Erdemozer01 <ozer246@gmail.com>
2025-01-27 03:18:07 +0300

    fixed

--- bayi/models.py
+++ bayi/models.py
@@ -6,6 +6,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 class SettingsSite(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name="Site Adı")
+    favicon = models.ImageField(upload_to="favicon", null=True, verbose_name="Favicon", blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
